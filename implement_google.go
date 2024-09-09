12044ab9904b25926de7d4e8de16a2cf48725b9a
juming <1471734250@qq.com>
2024-09-09 15:53:41 +0800

    新增谷歌登录实现

---
+++ implement_google.go
@@ -0,0 +1,124 @@
+package pkg_login
+
+import (
+	"encoding/json"
+	"errors"
+	"net/url"
+)
+
+/**
+ * Doc : https://developers.google.com/identity/protocols/oauth2/web-server?hl=zh-cn
+ */
+
+const (
+	GoogleRedirectPath = "https://accounts.google.com/o/oauth2/auth"     // 谷歌获取code地址
+	GoogleTokenPath    = "https://oauth2.googleapis.com/token"           // 谷歌获取token地址
+	GoogleUserInfoPath = "https://www.googleapis.com/oauth2/v2/userinfo" // 谷歌获取用户信息接口
+)
+
+type GoogleServer struct {
+}
+
+func newGoogleServer() *GoogleServer {
+	return &GoogleServer{}
+}
+
+func (g *GoogleServer) RedirectUrl() (string, error) {
+	parsedURL, err := url.Parse(GoogleRedirectPath)
+	if err != nil {
+		return "", err
+	}
+
+	queryParams := url.Values{}
+	queryParams.Add("response_type", "code")
+	queryParams.Add("client_id", config.GoogleId)
+	queryParams.Add("redirect_uri", config.GoogleRedirectUrl)
+	queryParams.Add("scope", "https://www.googleapis.com/auth/userinfo.profile")
+	queryParams.Add("access_type", "offline")
+
+	parsedURL.RawQuery = queryParams.Encode()
+
+	return parsedURL.String(), nil
+}
+
+type GoogleTokenResponse struct {
+	Error            string `json:"error"`
+	ErrorDescription string `json:"error_description"`
+	AccessToken      string `json:"access_token"`
+	ExpiresIn        int    `json:"expires_in"`
+	RefreshToken     string `json:"refresh_token"`
+	Scope            string `json:"scope"`
+	TokenType        string `json:"token_type"`
+	IDToken          string `json:"id_token"`
+}
+
+func (g *GoogleServer) token(code string) (string, error) {
+	payload := map[string]string{
+		"code":          code,
+		"client_id":     config.GoogleId,
+		"client_secret": config.GoogleSecret,
+		"redirect_uri":  config.GoogleRedirectUrl,
+		"grant_type":    "authorization_code",
+	}
+	payloadBytes, _ := json.Marshal(payload)
+
+	response, err := postBase(GoogleTokenPath, string(payloadBytes), nil)
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		_ = response.Body.Close()
+	}()
+
+	responseStruct := &GoogleTokenResponse{}
+	if err := json.NewDecoder(response.Body).Decode(responseStruct); err != nil {
+		return "", err
+	}
+
+	if len(responseStruct.ErrorDescription) != 0 {
+		return "", errors.New(responseStruct.ErrorDescription)
+	}
+
+	return responseStruct.AccessToken, nil
+}
+
+type GoogleUserInfo struct {
+	Id      string `json:"id"`
+	Name    string `json:"name"`
+	Picture string `json:"picture"`
+	Error   struct {
+		Code    int64  `json:"code"`
+		Message string `json:"message"`
+	} `json:"error"`
+}
+
+func (g *GoogleServer) GetUserinfo(code string) (*Userinfo, error) {
+	token, err := g.token(code)
+	if err != nil {
+		return nil, err
+	}
+
+	headers := map[string]string{"Authorization": "Bearer " + token}
+	response, err := getBase(GoogleUserInfoPath, headers)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = response.Body.Close()
+	}()
+
+	responseStruct := &GoogleUserInfo{}
+	if err := json.NewDecoder(response.Body).Decode(responseStruct); err != nil {
+		return nil, err
+	}
+
+	if responseStruct.Error.Code != 0 {
+		return nil, errors.New(responseStruct.Error.Message)
+	}
+
+	return &Userinfo{
+		Openid:   responseStruct.Id,
+		NickName: responseStruct.Name,
+		Avatar:   responseStruct.Picture,
+	}, nil
+}
--- server.go
+++ server.go
@@ -1,14 +1,74 @@
 package pkg_login
 
-import "fmt"
+import "errors"
+
+const (
+	ImplementGoogle   int8 = 1
+	ImplementWeiXin   int8 = 2
+	ImplementGithub   int8 = 3
+	ImplementQq       int8 = 4
+	ImplementWeiBo    int8 = 5
+	ImplementDingDing int8 = 6
+)
+
+var (
+	config  Config // 全局配置
+	hasInit bool   // 配置是否初始化
+)
+
+type Config struct {
+	GoogleId          string `json:"google_id"`
+	GoogleSecret      string `json:"google_secret"`
+	GoogleRedirectUrl string `json:"google_redirect_url"`
+}
+
+type Userinfo struct {
+	Openid   string `json:"openid"`
+	NickName string `json:"nick_name"`
+	Avatar   string `json:"avatar"`
+}
+
+type Ability interface {
+	RedirectUrl() (string, error)
+	GetUserinfo(code string) (*Userinfo, error)
+}
 
 type Server struct {
+	client      Ability
+	ImplementId int8 `json:"implement_id"`
 }
 
-func NewServer() *Server {
-	return &Server{}
+func Init(conf Config) {
+	config = conf
+	hasInit = true
 }
 
-func (s *Server) Call() {
-	fmt.Println("这是登录功能封装")
+func NewServer(implementId int8) (*Server, error) {
+	if !hasInit {
+		return nil, errors.New("配置未初始化,请先调用【Init】方法")
+	}
+
+	var client Ability
+	switch implementId {
+	case ImplementGoogle:
+		if len(config.GoogleId) == 0 || len(config.GoogleSecret) == 0 || len(config.GoogleRedirectUrl) == 0 {
+			return nil, errors.New("缺失配置文件")
+		}
+		client = newGoogleServer()
+	default:
+		return nil, errors.New("未定义实现")
+	}
+
+	return &Server{
+		client:      client,
+		ImplementId: implementId,
+	}, nil
+}
+
+func (s *Server) RedirectUrl() (string, error) {
+	return s.client.RedirectUrl()
+}
+
+func (s *Server) GetUserinfo(code string) (*Userinfo, error) {
+	return s.client.GetUserinfo(code)
 }
